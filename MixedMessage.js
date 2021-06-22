f1be170102960417bf910f05d63eef14f93b8d0c
B-Vermeulen <86245590+B-Vermeulen@users.noreply.github.com>
2021-06-22 18:44:38 +0200

    Fixed the code errors so it works properly

--- MixedMessage.js
+++ MixedMessage.js
@@ -2,11 +2,11 @@ const discounts = ["10%", "15%", "20%", "25%", "30%", "40%", "50%"];
 const items = ["toys", "airfryers", "magnetrons", "ovens", "footballs", "dolls"];
 
 function randomIndex(array) {
-    return Math.floor(math.random() * array.length);
+    return Math.floor(Math.random() * array.length);
 };
 
 function fullString() {
-    let fullString = `The ${items[randomIndex(items)]} have a discount of ${discounts[randomIndex(discounts)]}, be quick there are only ${math.floor(1 + math.random() * 10)} left!`
+    let fullString = `The ${items[randomIndex(items)]} have a discount of ${discounts[randomIndex(discounts)]}, be quick there are only ${Math.floor(1 + Math.random() * 100)} left!`
     console.log(fullString);
 };
 
